75196df8cb5efbff5a988e1308192f4755bdb92c
deadend-g4mes <leighton.h.carpenter@gmail.com>
2024-09-23 02:47:54 -0500

    functional

--- logic.py
+++ logic.py
@@ -1,1 +1,165 @@
+from pyscript import fetch, document
+import re
+import asyncio
+import json
+
+url='https://api.datamuse.com/words?'
+rhymes_struct = {}
+poem = []
+rhyming_words = []
 colors = ["Gold", "MediumTurquoise", "LightCoral", "LightGreen", "LawnGreen", "LightPink", "LightSalmon", "LightSkyBlue", "Orange", "Violet", "Plum", "PaleTurquoise", "Bisque"]
+
+async def fetch_rhymes(word, option):
+    datamuse_response = []
+    rhymes_list = []
+
+    if option == 0:
+        response = await fetch(url + "rel_rhy=" + word, method="GET")
+        datamuse_response = response.json()
+    
+    elif option == 1:
+        response = await fetch(url + "rel_nry=" + word, method="GET")
+        datamuse_response = response.json()
+    
+    else:
+        response_nry = await fetch(url + "rel_nry=" + word, method="GET")
+        response_rhy = await fetch(url + "rel_rhy=" + word, method="GET")
+        datamuse_response = await response_nry.json() + await response_rhy.json()
+
+    for entry in datamuse_response:
+        rhymes_list.append(entry['word'])
+
+    return rhymes_list
+
+#generates highlighted poem
+async def generate_rhyme_struct(input_text, option):
+    global rhyming_words
+    rhymes_struct.clear()
+    rhyming_words.clear()
+    poem.clear()
+    poem_length = len(re.split(r'\s+', input_text))
+    word_counter = 0
+    percentage = 0 
+    lines = re.split(r'\s+', input_text)
+    for line in lines:
+        poem.append(line.split(' '))
+
+    #looks for a word in the rhymes_struct, and returns the key if it exists
+    def exists(word):
+        for key, values in rhymes_struct.items():
+            if word in values:
+                return {"state": True, "key": key}
+        return {"state": False}
+    
+    count = 0
+    for test_line, words in enumerate(poem):
+        for test_word_index, test_word in enumerate(words):
+
+            if not poem[test_line]:
+                break
+
+            test_word = re.sub(r'[^A-Za-z0-9_]', '', test_word).lower()
+            first_test_word = exists(test_word)
+            
+            if not first_test_word["state"]:
+                fetch_rhymes_task = asyncio.create_task(fetch_rhymes(test_word, option))
+                rhyming_words = await fetch_rhymes_task
+
+                for line_index, line in enumerate(poem):
+                    for word_index, word in enumerate(line):
+                        word = re.sub(r'[^A-Za-z0-9_]', '', word).lower()
+
+                        if word in rhyming_words:
+                            test_word_exists = exists(test_word)
+                            word_exists = exists(word)
+
+                            if test_word_exists["state"] and word_exists["state"]:
+                                continue
+                            elif test_word_exists["state"]:
+                                rhymes_struct[test_word_exists["key"]].append(word)
+                            elif word_exists["state"]:
+                                rhymes_struct[word_exists["key"]].append(test_word)
+                            else:
+                                count += 1
+                                rhymes_struct[count] = [test_word, word]
+            
+            #progress bar
+            word_counter += 1
+            percentage = (word_counter * 100) / poem_length
+            document.querySelector('#progressbarText').innerText = str(word_counter) + ' / ' + str(poem_length) + ' words'
+            document.querySelector('#progressbarPercentage').innerText = str(round(percentage)) + '%'
+            document.querySelector('#progressbarInner').style.width = str(percentage) + "%"
+
+    return rhymes_struct
+
+def colorize_index(index):
+    while index >= len(colors):
+        index -= len(colors)
+    return colors[index]
+
+
+def colorize_words(matching_word_list, line_start, line_end, color_index):
+    for i in range(line_start, line_end - 1):
+        for j in range(len(poem[i])):
+            if isinstance(poem[i][j], str) and re.sub(r'[^A-Za-z0-9_]', '', poem[i][j]).lower() in matching_word_list:
+                poem[i][j] = {"word": poem[i][j], "color": colorize_index(color_index)}
+
+def match_lines(lines):
+    color_index = 0
+    count = 0
+    lines_block = []
+
+    if not poem[-1]:
+        poem.pop()
+    
+    for i, line in enumerate(poem):
+        if line != "" and len(line) != 0:
+            lines_block.extend(line)
+            count += 1
+        else:
+            continue
+        if count == lines or i == len(poem) - 1:
+            lines_block = [re.sub(r'[^A-Za-z0-9_]', '', word).lower() for word in lines_block]
+
+            for key, rhyming_words in rhymes_struct.items():
+                matching_words = [word for word in rhyming_words if word in lines_block]
+                if matching_words and len(matching_words) > 1:
+                    color_index += 1
+                    colorize_words(matching_words, 0, i + 2, color_index)
+            
+            count = 0
+            lines_block = []
+
+
+def match_stanzas():
+    color_index = 0
+    count = 0
+    lines_block = []   
+
+    poem.append([])
+    for i, line in enumerate(poem):
+        if line != "" and len(line) != 0:
+            lines_block.extend(line)
+        else:
+            lines_block = [re.sub(r'[^A-Za-z0-9_]', '', word).lower() for word in lines_block]
+            for key, rhymes in rhymes_struct.items():
+                matching_words = [word for word in rhymes if word in lines_block]
+                if matching_words and len(matching_words) > 1:
+                    color_index += 1
+                    colorize_words(matching_words, i-count, i+1, color_index)
+
+def match_rhyming_words(rhyming_method, lines):
+    if rhyming_method == 0:
+        match_stanzas()
+    elif rhyming_method == 1:
+        if not lines:
+            match_lines(4)
+        else:
+            match_lines(lines)
+    else:
+        #total number of words across all lines
+        total_words = sum(len(line) for line in poem)
+        match_lines(total_words)
+
+def return_poem():
+    return poem
--- main.py
+++ main.py
@@ -1,37 +1,119 @@
-# imports
-from logic import colors
+#imports
+from logic import colors, generate_rhyme_struct, match_rhyming_words, return_poem
 import random
-from pyscript import document, when
+from pyscript import when, document, fetch
+import asyncio
 
-# variables
-analyze_button = document.querySelector('.analyze-button')
-logo_text = document.querySelector('.logo-text')
-spinner = document.querySelector('#spinner')
-progressbar = document.querySelector('#progressbar')
-rectangles = spinner.querySelectorAll("div")
-btns = document.querySelectorAll('.btn')
+#variables
+analyze_button = document.getElementById('analyze-button')
+logo_text = document.getElementsByClassName('logo-text')[0]
+spinner = document.getElementById('spinner')
+progressbar = document.getElementById('progressbar')
+rectangles = spinner.getElementsByTagName("div")
+btns = document.getElementsByClassName('btn')
 random_color = random.choice(colors)
-textarea = document.querySelector('#text')
-output_container = document.querySelector('.output-container')
-output = document.querySelector('.output')
-reset_button = document.querySelector('#reset-button')
-text_reset = document.querySelector('#text-reset')
+textarea = document.getElementById('text')
+output_container = document.getElementsByClassName('output-container')[0]
+output = document.getElementsByClassName('output')[0]
+reset_button = document.getElementById('reset-button')
+text_reset = document.getElementById('text-reset')
 
-# Set a random color from the colors list as the logo's background color
+#set a random color from the colors list as the logo's background color
 logo_text.style.background = "linear-gradient(to top," + random_color +" 30%, transparent 30%)"
-# Set a random color from the colors list as the progressbar's color
+#set a random color from the colors list as the progressbar's color
 progressbar.style.backgroundColor = random_color
-# Set a random color from the colors list as loading spinner's color
+#set a random color from the colors list as loading spinner's color
 for rect in rectangles:
     rect.style.backgroundColor = random_color
-# Set a random color from the colors list as .btn background color
+#set a random color from the colors list as .btn background color
 for btn in btns:
     btn.style.backgroundColor = random_color
-# Run on load to get right textarea height when page is refreshed
+#run on load to get right textarea height when page is refreshed
 textarea.style.height = ""
 textarea.style.height = str(textarea.scrollHeight + 3) + "px"
 
-@when("input", textarea)
-def userTyping(e):
+#get and convert an element's id as a string
+def get_id(e):
+    return "#" + e.getAttribute("id")
+
+#replaces user inputted poem with rhyme highlighted version
+def display_poem():
+    
+    #fetches highlighted poem from logic.py
+    poem = return_poem()
+    
+    #seperates highlighted poem into lines
+    for line in poem:
+        
+        #seperates each word from the lines
+        for word_info in line:
+
+            #creates new text element in input box
+            word_span = document.createElement('span')
+            empty_span = document.createElement('span')
+            empty_span.textContent = ' '
+            output.appendChild(word_span)
+            output.appendChild(empty_span)
+            
+            if isinstance(word_info, str):
+                word_span.textContent = word_info
+                
+            else:
+                word_span.setAttribute('style', 'background-color:' + word_info['color'])
+                word_span.textContent = word_info['word']
+                
+        output.appendChild(document.createElement('br'))
+        
+    output_container.classList.remove("hidden")
+    textarea.classList.add("hidden")
+
+#start rhymepy
+@when("click", get_id(analyze_button))
+async def anal_btn(e):
+    textValue = textarea.value
+    rhymingOption = document.querySelector('input[name="rhyming-options"]:checked').value 
+    rhymingMethod = document.querySelector('input[name="rhyming-methods"]:checked').value
+    lines = document.querySelector('input[name="lines-value"]').value
+    
+    #hide output and show input box
+    output_container.classList.add("hidden")
+    textarea.classList.remove("hidden")
+
+    #check if textarea is not empty
+    if len(textValue.strip()) != 0:
+        analyze_button.disabled = True
+        output.innerHTML = ""
+        spinner.classList.remove("hidden")
+        progressbar.classList.remove("hidden")
+        await generate_rhyme_struct(textValue, rhymingOption)
+        match_rhyming_words(rhymingMethod, lines)
+        display_poem()
+        spinner.classList.add("hidden")
+        progressbar.classList.add("hidden")
+        analyze_button.disabled = False
+        progressbar.style.width = "1%"
+
+#resize input box as user inputs lines
+@when("input", get_id(textarea))
+def user_typing(e):
     textarea.style.height = ""
     textarea.style.height = str(textarea.scrollHeight) + "px"
+    
+#reset button
+@when("click", get_id(reset_button))
+def reset_btn(e):
+    output_container.classList.add("hidden")
+    textarea.classList.remove("hidden")
+    progressbar.classList.add("hidden")
+
+#reset text
+@when("click", get_id(reset_button))
+def text_res(e):
+    
+    #Reset textarea height
+    textarea.value = ""
+    textarea.style.height = ""
+    textarea.style.height = textarea.scrollHeight + 3 + "px"
+
+    output_container.classList.add("hidden")
+    textarea.classList.remove("hidden")
